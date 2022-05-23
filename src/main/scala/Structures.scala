a66cb5aee85cd38649d6701b41b027599b3fe88d
FabienCeppic <flast.ceppic.cdi22@gmail.com>
2022-05-23 14:19:20 +0200

    for loop

--- src/main/scala/Structures.scala
+++ src/main/scala/Structures.scala
@@ -10,4 +10,20 @@ object Structures extends App {
   else
     println("x est positif")
 
+  //boucle for
+  val ints = List(1, 2, 3, 4, 5)
+
+  for (i <- ints) println(i)
+
+  //guards ajoute condition
+  for (i <- ints if(i > 2)) println(i)
+
+  //yield stock chaque résultat d'une itération du for dans une liste
+  val doubles = for( i <- ints) yield i * 2
+  println(doubles)
+
+  //parcourir une liste
+  val names = List("chris", "ed", "maurice")
+  val capNames = for (name <- names) yield name.capitalize
+  println(capNames)
 }
