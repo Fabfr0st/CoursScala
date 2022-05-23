7d1e458421b9a962ec3394034de7e7210888de2a
FabienCeppic <flast.ceppic.cdi22@gmail.com>
2022-05-23 14:07:05 +0200

    condition

---
+++ src/main/scala/Structures.scala
@@ -0,0 +1,13 @@
+object Structures extends App {
+
+  //condition
+  val x = 0
+
+  if (x < 0)
+    println("x est négatif")
+  else if (x == 0)
+    println("x est égal à zéro")
+  else
+    println("x est positif")
+
+}
